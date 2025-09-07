5bd5d252b6e9d0c7a7982ed1db9fdc6a50526cc9
irisHanb <fortrulymylife@gmail.com>
2025-09-07 19:02:44 +0900

    front: next auth login logic 추가

---
+++ frontend/auth.ts
@@ -0,0 +1,62 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/prisma";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { compoarePassword } from "./src/lib/password-utils";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  useSecureCookies: process.env.NODE_ENV === "production",
+  trustHost: true,
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.AUTH_SECRET,
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: {
+          label: "이메일",
+          type: "email",
+          placeholder: "이메일 입력",
+        },
+        password: {
+          label: "비밀번호",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        // 1. validate credentials
+        if (!credentials || !credentials.email || !credentials.password) {
+          throw new Error("이메일과 비밀번호를 입력해주세요.");
+        }
+
+        // 2. get user from db
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email as string,
+          },
+        });
+
+        if (!user) {
+          throw new Error("존재하지 않는 이메일입니다.");
+        }
+
+        // 3. compare password
+        const isMatchPassword = compoarePassword(
+          credentials.password as string,
+          user.hashedPassword as string
+        );
+
+        if (!isMatchPassword) {
+          throw new Error("비밀번호가 일치하지 않습니다.");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  pages: {},
+  callbacks: {},
+});
